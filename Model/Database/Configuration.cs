339387c0aae2d5516b0de1558aac3645e49e825f
JaoHundred <43298295+JaoHundred@users.noreply.github.com>
2023-08-28 17:29:08 -0300

    preparing database infrastructure

---
+++ Model/Database/Configuration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Database
+{
+    //add litdb
+
+    public class Configuration
+    {
+    }
+}
