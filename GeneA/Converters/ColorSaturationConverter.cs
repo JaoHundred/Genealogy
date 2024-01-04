308b1a315b3b4a0b6fe3170d96b757b502ac7ab0
JaoHundred <43298295+JaoHundred@users.noreply.github.com>
2024-01-04 18:08:45 -0300

    Trying to make Pressed and PointOver node change its color based on original node color

---
+++ GeneA/Converters/ColorSaturationConverter.cs
@@ -0,0 +1,36 @@
+﻿using Avalonia.Data;
+using Avalonia.Data.Converters;
+using Avalonia.Media;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+
+namespace GeneA.Converters
+{
+    public class ColorSaturationConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if(value is SolidColorBrush solidColor)
+            {
+                var hsla = solidColor.Color.ToHsl();
+                var newColor = new HslColor(hsla.A, hsla.H, hsla.S * 0.8, hsla.L).ToRgb();
+
+                //TODO:color is blinking, see what is happening
+               return new SolidColorBrush(newColor);
+            }
+
+            return BindingOperations.DoNothing;
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
