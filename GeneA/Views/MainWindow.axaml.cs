1bffba4fa6213a72170278857f913fb176d23b74
JaoHundred <43298295+JaoHundred@users.noreply.github.com>
2024-05-14 12:07:02 -0300

    Initial deploy setup

--- GeneA/Views/MainWindow.axaml.cs
+++ GeneA/Views/MainWindow.axaml.cs
@@ -1,4 +1,8 @@
 ﻿using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Platform;
+using System;
+using System.Linq;
 
 namespace GeneA.Views;
 
@@ -6,7 +10,21 @@ public partial class MainWindow : Window
 {
     public MainWindow()
     {
+        ScreenResolution();
         InitializeComponent();
     }
-    
+
+    private void ScreenResolution()
+    {
+        var primaryScreen = Screens.All.FirstOrDefault();
+
+        if (primaryScreen != null)
+        {
+            double height = primaryScreen.Bounds.Height;
+            double width = primaryScreen.Bounds.Width;
+
+            Height = height / 3;
+            Width = width / 3;
+        }
+    }
 }
