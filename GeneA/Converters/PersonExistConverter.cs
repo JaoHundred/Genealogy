9f1a61ee01771bb737ef24d71a1d0d8e571e2832
JaoHundred <43298295+JaoHundred@users.noreply.github.com>
2024-07-25 13:02:39 -0300

    update to avalonia 11.1, new value converters, avalonia 11.1 does not support anymore conditional inside bindings in xaml

---
+++ GeneA/Converters/PersonExistConverter.cs
@@ -0,0 +1,27 @@
+﻿using Avalonia.Data.Converters;
+using ModelA.Core;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneA.Converters
+{
+    public class PersonExistConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is Person person)
+                return person.Id > Guid.Empty;
+            return false;
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GeneA/Converters/PersonNotExistConverter.cs
@@ -0,0 +1,26 @@
+﻿using Avalonia.Data.Converters;
+using ModelA.Core;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneA.Converters
+{
+    internal class PersonNotExistConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (value is Person person)
+                return person.Id == Guid.Empty;
+            return false;
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
